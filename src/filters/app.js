cb0be0f51ff58110f941cfe232cdec3e172a0108
Alvinyw <alvin.yiwen@gmail.com>
2023-07-12 18:59:45 +0800

    删除冗余代码

--- src/filters/app.js
+++ src/filters/app.js
@@ -1,17 +1,11 @@
-import {
-  appSetting
-} from '../setting/app'
 
-// 产品管理模块 的全局方法
 const appFilters = {
 
-  // 获取产品类型描述
-  filterBankCardType: function (val) {
-    if (!val) return ''
-    const _res = appSetting.bankCardtypes.find((value) => {
-      return value['value'] === Number(val)
-    });
-    return _res['label'] || ''
+  // 首字母大写
+  capitalize: function (value) {
+    if (!value) return ''
+    value = value.toString()
+    return value.charAt(0).toUpperCase() + value.slice(1)
   },
 
 }
--- src/main.js
+++ src/main.js
@@ -9,15 +9,10 @@ import ElementUI from 'element-ui'
 import '@/styles/theme/index.css'
 Vue.use(ElementUI)
 
-// 引入字体图标库 iconfont
-import '@/styles/iconfont/iconfont.css'
-import '@/styles/iconfont/iconfont.js'
-
 // import './utils/rem' // 适配移动端设备
 import './api' // 接口文件
 import './utils' // 工具库
 import './components/install' // 自定义全局组件
-import './setting' // 全局设置文件
 import './directive' // 自定义全局指令
 import './filters' // 自定义全局过滤器
 import './styles/index.scss' // 全局样式文件
